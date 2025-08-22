dbbb33ec86e622ddb8976ff0e85fc0e6010531ee
themintguy <bandakranthi31@gmail.com>
2025-08-22 18:33:51 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+
+const App = () => {
+  return (
+    <>
+    <h1>
+      <a href="kranthi.tech" target="_blank" rel="noopener noreferrer">
+        ME
+      </a>
+    </h1>
+    </>
+  )
+}
+
+export default App
